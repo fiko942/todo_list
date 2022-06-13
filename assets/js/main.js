33132cd2b2cbc55207eec98e55f0e170e08d86ac
fiko942 <tobellord@gmail.com>
2022-06-13 09:11:53 +0700

    Add feature: move list and delete list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const {ipcRenderer} = require('electron');
 const ipc = ipcRenderer;
 window.$ = window.jQuery = require('jquery');
-// window.$ = window.jQuery = require('./jquery-3.0.0.min.js');
+let dataTemp;
 
 const el = (q) => {
     return document.querySelector(q);
@@ -50,4 +50,104 @@ $('.add-task').on('click', (e) => {
     $('.modal-footer').css('cssText', `
         border-top: none !important;
     `);
-})
+});
+
+function showNotification(title = '', message = '') {
+    let toast = new bootstrap.Toast(document.querySelector('#live-toast'));
+    $('.toast-title').text(title.trim());
+    $('.toast-message').text(message.trim());
+    $('.toast-container').css('cssText', `
+            margin-left: 10px !important;
+            margin-top: -10px !important;
+        `);
+    $('body, html').css('cssText', `
+            overflow: hidden !important;
+        `);
+    toast.show();
+}
+
+$('#list-name-add').on('keyup', (e) => {
+    if(e.keyCode === 13) {
+        addList();
+    }
+});
+
+$("#submit-add-list").on('click', () => {
+    addList();
+});
+
+function addList() {
+    ipc.send('add-list', $('#list-name-add').val().trim());
+}
+
+ipc.on('message', (e, data) => {
+    showNotification(data[0], data[1]);
+});
+
+ipc.on('add-list-error', (e, data) => {
+    showNotification('Add list error', data);
+    $("#add-list").modal('hide');
+});
+
+ipc.on('add-list-complete', (e, data) => {
+    showNotification('Add list success', 'New list has been added successfully');
+    $("#add-list").modal('hide');
+    $("#list-name-add").val('');
+});
+
+ipc.on('list-data', (e, data) => {
+    let listContainer = document.querySelector('.task-item-container');
+    listContainer.innerHTML = '';
+    for(let i = data.length - 1; i > -1; i--) {
+        let el = document.createElement('div');
+        el.classList.add('task');
+        el.setAttribute('list-id', data[i]['id']);
+        el.classList.add('d-flex');
+        el.setAttribute('task-id', data[i]['id']);
+        el.innerHTML = `
+            <i class='bx bxs-calendar-star icon' ></i>
+            <span class="name">${data[i]['name']}</span>
+            <button class="delete" id="delete-list" style="z-index: 9999999999999999999999" list-id="${data[i]['id']}" list-name="${data[i]['name']}"><i class='bx bx-trash' ></i></button>
+            <span class="count"></span>
+        `;
+        if(i === data.length - 1) {
+            el.classList.add('active');
+        }
+        listContainer.appendChild(el);
+        document.querySelectorAll('.task-item-container .task').forEach(el => {
+            el.addEventListener('click', () => {
+                document.querySelectorAll('.task-item-container .task').forEach(elel => { elel.classList.remove('active'); });
+                el.classList.add('active');
+                let id = (el.hasAttribute('task-id')) ? el.getAttribute('task-id') : '';
+                ipc.send('open-task', id);
+                console.log(id);
+            });
+        });
+
+        document.querySelectorAll("#delete-list[list-id][list-name]").forEach(el => {
+            el.addEventListener('click', () => {
+                var data = {id: el.getAttribute('list-id'), name: el.getAttribute('list-name')};
+                dataTemp = data;
+                $('#confirm-delete-list-message').text(`Are you sure to delete list: ${data['name']} ?`);
+                $('#confirm-delete-list').modal('show');
+            });
+        });
+    }
+    $('.task-item-container .task')[0].click();
+});
+
+$('#continue-delete-list').on('click', () => {
+    ipc.send('delete-list', dataTemp);
+    $('#confirm-delete-list').modal('hide');
+});
+
+$('#add-task form').on('submit', (e) => {
+    e.preventDefault();
+    let name = $("#task-name-add").val().trim();
+    let date = $('#task-date-add').val();
+    let time = $("#task-time-add").val();
+    let note = $("#task-note-add").val().trim();
+    let list_id = $(".task.active").attr('list-id');
+    let data = {name, date, time, note, list_id};
+    ipc.send('add-task', data);
+});
--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const {
 } = require('electron');
 const ipc = ipcMain;
 const path = require('path');
+const Settings = require('electron-config');
+const settings = new Settings();
 let win;
 
 function createWindow () {
@@ -22,9 +24,15 @@ function createWindow () {
   win.loadFile('page/main.html');
 }
 
+function sendListData() {
+  win.webContents.send('list-data', settings.get('data.list'));
+}
+
 app.whenReady().then(() => {
   createWindow();
-
+  win.webContents.on('did-finish-load', () => {
+    sendListData();
+  });
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -68,3 +76,60 @@ ipc.on('close-app', () => {
     app.quit();
   }
 });
+
+function randomString(length = 1) {
+  let allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890____________';
+  let c = '';
+  for(let i = 0; i < length; i++) {
+    c += allowedChars[Math.floor(Math.random() * allowedChars.length)];
+  }
+  return c;
+}
+
+// Initialize data settings for
+// first using this application
+if(!settings.has('data')) {
+  settings.set('data', {
+    list: [],
+    task: []
+  });
+}
+
+ipc.on('add-list', (e, data) => { 
+  let listname = data.replace('<', '').replace('>', '').replace(';', '');
+  if(listname.trim().length < 1) {
+    win.webContents.send('add-list-error', 'Please enter list name at least 1 characters');
+    sendListData();
+  } else if(listname.trim().length > 30) {
+    win.webContents.send('add-list-error', 'Please enter list name less than 30 characters');
+    sendListData();
+  } else {
+    let dataBefore = settings.get('data')['list'];
+    let randomId = randomString(25);
+    dataBefore.push({id: randomId, name: listname});
+    settings.set('data.list', dataBefore);
+    win.webContents.send('add-list-complete', null);
+    sendListData();
+  }
+});
+
+ipc.on('delete-list', (e, data) => {
+  var list = settings.get('data')['list'];
+  var arr = [];
+  for(let i = 0; i < list.length; i++) {
+    if(list[i]['id'] != data['id']) {
+      arr.push(list[i]);
+    }
+  }
+  settings.set('data.list', arr);
+  sendListData();
+});
+
+ipc.on('add-task', (e, data) => {
+  console.log(data);
+});
+
+ipc.on('open-task', (e, id) => {
+  console.log(id);
+});
+
